4a7f46bb62a08028e3105ee70901209c3203b1f9
Douglas Urner <dlu@canishe.com>
2018-03-05 12:41:17 -0800

    Added more colliders.

--- Pac-Person-Manual-Maze/Assets/Scripts/PacmanMove.cs
+++ Pac-Person-Manual-Maze/Assets/Scripts/PacmanMove.cs
@@ -18,24 +18,27 @@ public class PacmanMove : MonoBehaviour {
 		Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
 		GetComponent<Rigidbody2D>().MovePosition(p);
 
-		if ((Vector2) transform.position == dest) {
-			if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up)) {
-				dest = (Vector2) transform.position + Vector2.up;
-			} 
-			if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right)) {
-				dest = (Vector2) transform.position + Vector2.right;
-				Debug.Log("right");
-			} 
-			if (Input.GetKey(KeyCode.DownArrow) && valid(Vector2.down)) {
-				dest = (Vector2) transform.position - Vector2.up;
-			} 
-			if (Input.GetKey(KeyCode.LeftArrow) && valid(Vector2.left)) {
-				dest = (Vector2) transform.position - Vector2.right;
-			}
+		if ((Vector2) transform.position != dest) {
+			// Already moving
+			return;
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow) && validMove(Vector2.up)) {
+			dest = (Vector2) transform.position + Vector2.up;
+		}
+		if (Input.GetKey(KeyCode.RightArrow) && validMove(Vector2.right)) {
+			dest = (Vector2) transform.position + Vector2.right;
+			Debug.Log("right");
+		}
+		if (Input.GetKey(KeyCode.DownArrow) && validMove(Vector2.down)) {
+			dest = (Vector2) transform.position + Vector2.down;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow) && validMove(Vector2.left)) {
+			dest = (Vector2) transform.position + Vector2.left;
 		}
 	}
 
-	bool valid(Vector2 dir) {
+	bool validMove(Vector2 dir) {
 		Vector2 pos = transform.position;
 		RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
 		bool valid = (hit.collider == GetComponent<Collider2D>());
