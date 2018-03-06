81a12cc6ffebdd312c73160ef817a68c363235b8
Douglas Urner <dlu@canishe.com>
2018-03-06 07:24:56 -0800

    Add PacDot sprite and script.

---
+++ Pac-Person-Hand-Made-Maze/Assets/Scripts/PacDot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacDot : MonoBehaviour {
+//	public int dotsEaten = 0;
+//
+//	private void OnTriggerEnter2D(Collider2D other) {
+//		if (other.CompareTag("Player")) {
+//			Destroy(gameObject);
+//			dotsEaten++;
+//		}
+//	}
+
+	// Use this for initialization
+	void Start () {
+		gameObject.tag = "PacDot";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
