bc52ee4e067c756a3ef6a39328ec2f5c1a29cdca
Douglas Urner <dlu@canishe.com>
2018-03-07 00:06:58 -0800

    Rename script, add strategy switch.

--- Pac-Person-Hand-Made-Maze/Assets/Scripts/GhostAI.cs
+++ Pac-Person-Hand-Made-Maze/Assets/Scripts/GhostAI.cs
@@ -2,27 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlinkyAI : MonoBehaviour {
+public class GhostAI : MonoBehaviour {
+	// TODO: make strategy an enum.
+	public int strategy = 0;
 	public Transform[] waypoints;
 	public int cur = 0;
 	public float speed = 0.3f;
 
+	Vector2 dest = Vector2.zero;
+
 	// Use this for initialization
 	void Start () {
-		
+		dest = transform.position;
 	}
 	
 	// Update is called once per tick
-	void FixedUpdate () {
-		// Waypoint not reached yet? then move closer
-		if (transform.position != waypoints[cur].position) {
-			Vector2 p = Vector2.MoveTowards(transform.position,
-				waypoints[cur].position,
-				speed);
+	void FixedUpdate() {
+		if ((Vector2) transform.position != dest) {
+			Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
 			GetComponent<Rigidbody2D>().MovePosition(p);
 		}
-		// Waypoint reached, select next one
-		else cur = (cur + 1) % waypoints.Length;
+
+		switch (strategy) {
+			case 0:
+				// Waypoints
+				if ((Vector2) transform.position == dest) {
+					cur = (cur + 1) % waypoints.Length;
+					dest = waypoints[cur].position;
+				}
+
+			break;
+		}
 
 		// Animation
 		Vector2 dir = waypoints[cur].position - transform.position;
@@ -31,7 +41,7 @@ public class BlinkyAI : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D co) {
-		if (co.name == "pacman")
+		if (co.CompareTag("Player"))
 			Destroy(co.gameObject);
 	}
 }
